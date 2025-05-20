281aeba658ae6eadb5870cefb0a37c3a83a2c63b
I <abstract.girl@gmail.com>
2025-05-20 02:00:40 +0200

    улучшено логирование

--- train.py
+++ train.py
@@ -25,6 +25,10 @@ from models import create_model
 from util.visualizer import Visualizer
 import mlflow
 import os
+import numpy as np
+import io
+import torch
+
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
@@ -72,6 +76,11 @@ if __name__ == '__main__':
                 epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
                 visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
                 model.update_learning_rate()    # update learning rates in the beginning of every epoch.
+                
+                # Log learning rate at the beginning of each epoch
+                if opt.use_mlflow:
+                    mlflow.log_metric("learning_rate", model.optimizers[0].param_groups[0]['lr'], step=epoch)
+                
                 for i, data in enumerate(dataset):  # inner loop within one epoch
                     iter_start_time = time.time()  # timer for computation per iteration
                     if total_iters % opt.print_freq == 0:
@@ -85,7 +94,9 @@ if __name__ == '__main__':
                     if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
                         save_result = total_iters % opt.update_html_freq == 0
                         model.compute_visuals()
-                        visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                        visuals = model.get_current_visuals()
+                        visualizer.display_current_results(visuals, epoch, save_result)
+                        
 
                     if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                         losses = model.get_current_losses()
@@ -94,19 +105,33 @@ if __name__ == '__main__':
                         if opt.display_id > 0:
                             visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
                         
-                        # Log metrics to MLflow if enabled and autolog is not enabled
-                        # (autolog will handle the metrics automatically)
-                        if opt.use_mlflow and not opt.mlflow_autolog:
+                        # Always log metrics to MLflow at each print_freq, regardless of autolog setting
+                        if opt.use_mlflow:
                             for loss_name, loss_value in losses.items():
                                 mlflow.log_metric(f"loss/{loss_name}", loss_value, step=total_iters)
                             mlflow.log_metric("time/data", t_data, step=total_iters)
                             mlflow.log_metric("time/compute", t_comp, step=total_iters)
-                            mlflow.log_metric("learning_rate", model.optimizers[0].param_groups[0]['lr'], step=total_iters)
+                            mlflow.log_metric("progress/epoch", epoch, step=total_iters)
+                            mlflow.log_metric("progress/iteration", total_iters, step=total_iters)
 
                     if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
                         print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
                         save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
                         model.save_networks(save_suffix)
+                        
+                        # Log latest model checkpoint to MLflow
+                        if opt.use_mlflow:
+                            checkpoint_files = []
+                            for net_name in model.model_names:
+                                save_filename = '%s_net_%s.pth' % (save_suffix, net_name)
+                                save_path = os.path.join(opt.checkpoints_dir, opt.name, save_filename)
+                                if os.path.exists(save_path):
+                                    checkpoint_files.append(save_path)
+                            
+                            # Log checkpoints as artifacts
+                            for ckpt_file in checkpoint_files:
+                                artifact_path = f"checkpoints/iter_{total_iters}"
+                                mlflow.log_artifact(ckpt_file, artifact_path=artifact_path)
 
                     iter_data_time = time.time()
                 if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
@@ -114,17 +139,22 @@ if __name__ == '__main__':
                     model.save_networks('latest')
                     model.save_networks(epoch)
                     
-                    # Log model to MLflow if enabled and autolog is not enabled
-                    # (autolog will handle model logging automatically)
-                    if opt.use_mlflow and not opt.mlflow_autolog:
+                    # Log epoch model to MLflow
+                    if opt.use_mlflow:
                         # Save checkpoints directory to MLflow
                         checkpoint_dir = os.path.join(opt.checkpoints_dir, opt.name)
-                        mlflow.log_artifacts(checkpoint_dir, artifact_path="checkpoints")
+                        for net_name in model.model_names:
+                            save_filename = '%s_net_%s.pth' % (epoch, net_name)
+                            save_path = os.path.join(checkpoint_dir, save_filename)
+                            if os.path.exists(save_path):
+                                mlflow.log_artifact(save_path, artifact_path=f"checkpoints/epoch_{epoch}")
 
-                print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
-                # Log epoch metrics to MLflow if enabled and autolog is not enabled
-                if opt.use_mlflow and not opt.mlflow_autolog:
-                    mlflow.log_metric("epoch/duration_seconds", time.time() - epoch_start_time, step=epoch)
+                epoch_time = time.time() - epoch_start_time
+                print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, epoch_time))
+                # Log epoch metrics to MLflow
+                if opt.use_mlflow:
+                    mlflow.log_metric("epoch/duration_seconds", epoch_time, step=epoch)
+                    mlflow.log_metric("epoch/completed", epoch, step=epoch)
     else:
         # Original training code without MLflow
         dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
